f0b0afdaa7c8f88703e061c399dc07b8b51fdaf6
Ayush Jadhav <ayushjadhav204204@gmail.com>
2026-09-15 15:22:41 +0530

    Add complete project files and setup

---
+++ frontend/E-Bookcreater/src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import React, { Children } from 'react'
+import { Navigate, useLocation } from 'react-router-dom';
+
+const ProtectedRoute = ({children}) => {
+const isAuthenticated = true;
+const loading = false;
+const location = useLocation();
+
+if(loading){
+    //you can add a loading spinner here if you want 
+    return <div > Loading...</div>;
+}
+
+if(!isAuthenticated){
+    return <Navigate to="login" state={{from:location}}replace/>;
+}
+ return children;   
+}
+
+export default ProtectedRoute
